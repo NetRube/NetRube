6e8ed28fa5bc14d5f850b90606b41edf4c0fd82a
NetRube <netrube@126.com>
2013-07-30 14:43:22 +0800

    增加单例调用方法

---
+++ NetRube/Singleton.cs
@@ -0,0 +1,11 @@
+﻿
+namespace NetRube
+{
+	/// <summary>以单例模式调用 <typeparamref name="T"/></summary>
+	/// <typeparam name="T">要使用单例的类型</typeparam>
+	public static class Singleton<T> where T : new()
+	{
+		/// <summary><typeparamref name="T"/> 的实例</summary>
+		public static T Instance = new T();
+	}
+}
